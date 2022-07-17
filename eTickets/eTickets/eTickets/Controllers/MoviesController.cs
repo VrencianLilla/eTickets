1b6ca7cdb0939230e8bb6d84d3441e9823b8796c
VrencianLilla <79305052+VrencianLilla@users.noreply.github.com>
2022-07-17 20:00:20 +0300

    Create MoviesController

---
+++ eTickets/eTickets/eTickets/Controllers/MoviesController.cs
@@ -0,0 +1,20 @@
+﻿using eTickets.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eTickets.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public MoviesController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var data = _context.Movies.ToList();
+            return View();
+        }
+    }
+}
